6334ed64fd8a299321b99cb8b770b6616188e9bb
oTranThanhNghia <thanhnghiaglhn92@gmail.com>
2023-07-06 14:58:47 +0700

    example react-native-hold-menu without expo libs

--- App.tsx
+++ App.tsx
@@ -9,110 +9,36 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import Basic from './src/Basic';
+// import Basic from './src/Basic';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const MenuItems = [
+  {text: 'Actions', icon: 'home', isTitle: true, onPress: () => {}},
+  {text: 'Action 1', icon: 'edit', onPress: () => {}},
+  {text: 'Action 2', icon: 'map-pin', withSeparator: true, onPress: () => {}},
+  {text: 'Action 3', icon: 'trash', isDestructive: true, onPress: () => {}},
+];
+
+import {HoldMenuProvider, HoldItem} from 'react-native-hold-menu';
 
 const App = () => {
-  // const isDarkMode = useColorScheme() === 'dark';
-
-  // const backgroundStyle = {
-  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  // };
-
-  return <Basic />;
-
-  // return (
-  //   <SafeAreaView style={backgroundStyle}>
-  //     <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-  //     <ScrollView
-  //       contentInsetAdjustmentBehavior="automatic"
-  //       style={backgroundStyle}>
-  //       <Header />
-  //       <View
-  //         style={{
-  //           backgroundColor: isDarkMode ? Colors.black : Colors.white,
-  //         }}>
-  //         <Section title="Step One">
-  //           Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-  //           screen and then come back to see your edits.
-  //         </Section>
-  //         <Section title="See Your Changes">
-  //           <ReloadInstructions />
-  //         </Section>
-  //         <Section title="Debug">
-  //           <DebugInstructions />
-  //         </Section>
-  //         <Section title="Learn More">
-  //           Read the docs to discover what to do next:
-  //         </Section>
-  //         <LearnMoreLinks />
-  //       </View>
-  //     </ScrollView>
-  //   </SafeAreaView>
-  // );
+  return (
+    <HoldMenuProvider theme="light">
+      {/* Your app components */}
+      <View style={{flex: 1}}>
+        <HoldItem items={MenuItems}>
+          <View style={{height: 30, width: 30, backgroundColor: 'red'}} />
+        </HoldItem>
+        <HoldItem items={MenuItems}>
+          <View style={{height: 30, width: 30, backgroundColor: 'blue'}} />
+        </HoldItem>
+        <HoldItem items={MenuItems} menuAnchorPosition="bottom-right">
+          <View style={{height: 30, width: 30, backgroundColor: 'yellow'}} />
+        </HoldItem>
+      </View>
+    </HoldMenuProvider>
+  );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
